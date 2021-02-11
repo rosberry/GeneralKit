549d7aac447820290447d80a49b5a64ce38a541e
ntunin <nikolay.tyunin@rosberry.com>
2021-02-11 12:12:48 +0600

    Update Plugin.swift

--- Sources/GeneralKit/Models/Plugin.swift
+++ Sources/GeneralKit/Models/Plugin.swift
@@ -6,12 +6,10 @@ public struct Plugin: Codable, Equatable, CustomStringConvertible {
     public let name: String
     public let commands: [String]
     public let repo: String
-    public let package: String
 
-    public init(name: String, commands: [String], repo: String, package: String) {
+    public init(name: String, commands: [String], repo: String) {
         self.name = name
         self.commands = commands
         self.repo = repo
-        self.package = package
     }
 }
